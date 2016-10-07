380e5dc09df73a4a28a593637dbcf4b9dd99f87e
Chris Mytton <chrismytton@gmail.com>
2016-10-07 16:20:02 +0100

    Move tests into the test directory

--- morph_scraper-database.gemspec
+++ morph_scraper-database.gemspec
@@ -25,4 +25,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'minitest', '~> 5.0'
   spec.add_development_dependency 'pry', '~> 0.10'
   spec.add_development_dependency 'rubocop', '~> 0.43'
+  spec.add_development_dependency 'webmock', '~> 2.1'
 end
--- test/morph_scraper/database_test.rb
+++ test/morph_scraper/database_test.rb
@@ -4,4 +4,45 @@ describe MorphScraper::Database do
   it 'has a version number' do
     ::MorphScraper::Database::VERSION.wont_be_nil
   end
+
+  def with_tmp_dir(&block)
+    Dir.mktmpdir do |tmp_dir|
+      Dir.chdir(tmp_dir, &block)
+    end
+  end
+
+  before do
+    stub_request(:get, 'https://morph.io/chrismytton/denmark-folketing-wikidata/data.sqlite?key=secret')
+      .to_return(body: 'remote data')
+  end
+
+  subject { MorphScraper::Database.new('chrismytton/denmark-folketing-wikidata', api_key: 'secret') }
+
+  it 'copies the remote database into the current directory' do
+    with_tmp_dir do
+      File.exist?('data.sqlite').must_equal false
+      subject.copy
+      File.exist?('data.sqlite').must_equal true
+      File.read('data.sqlite').must_equal 'remote data'
+    end
+  end
+
+  it "doesn't overwrite an existing database" do
+    with_tmp_dir do
+      FileUtils.touch('data.sqlite')
+      -> { subject.copy }.must_raise MorphScraper::Database::Error
+    end
+  end
+
+  describe 'with force: true option' do
+    subject { MorphScraper::Database.new('chrismytton/denmark-folketing-wikidata', api_key: 'secret', force: true) }
+
+    it 'overwrites an existing database' do
+      with_tmp_dir do
+        File.write('data.sqlite', 'existing')
+        subject.copy
+        File.read('data.sqlite').must_equal 'remote data'
+      end
+    end
+  end
 end
